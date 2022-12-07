660b7255120c284e47bb444104215abee7591d72
scarf <greenscarf005@gmail.com>
2022-12-08 01:41:32 +0900

    feat: solve 01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,80 @@
+import com.quickbirdstudios.nonEmptyCollection.list.NonEmptyList
+import com.quickbirdstudios.nonEmptyCollection.unsafe.UnsafeNonEmptyCollectionApi
+import com.quickbirdstudios.nonEmptyCollection.unsafe.toNonEmptyList
+import java.io.BufferedReader
 import java.io.File
-import java.nio.file.Paths
+import java.net.URL
 
 fun main(args: Array<String>) {
-    val fileName = if (args.isNotEmpty()) args[0] else "src/main/resources/01.txt"
+//    val url = input(1)
+    println(solve02(example(2)))
+}
 
-    val file = File(fileName)
-    val values = file
-        .readLines()
-        .splitWhen { it.isBlank() }
-        .map { array -> array.map (String::toInt)  }
 
-    println(values)
+typealias TestData = NonEmptyList<String>
+
+fun Int.twoDigit() = if (this < 10) "0$this" else "$this"
+
+@OptIn(UnsafeNonEmptyCollectionApi::class)
+fun example(day: Int) = File("src/main/resources/${day.twoDigit()}.txt").readLines().toNonEmptyList()
+
+@OptIn(UnsafeNonEmptyCollectionApi::class)
+fun input(day: Int): TestData {
+    val url = URL("https://adventofcode.com/2022/day/$day/input")
+    val connection = url.openConnection().apply {
+        setRequestProperty("Cookie", "session=${System.getenv("AOC_SESSION")}")
+    }
+    // add header
+    return BufferedReader(connection.getInputStream().reader()).readLines().toNonEmptyList()
+}
+
+typealias Solve<T> = (TestData) -> T
+
+val solve01: Solve<Int> = {
+    it
+        .splitWhen(String::isBlank)
+        .map { array -> array.map(String::toInt) }
+        .maxOf { it.sum() }
+}
+
+val solve01_2: Solve<Int> = { data ->
+    data
+        .splitWhen(String::isBlank)
+        .asSequence()
+        .map { array -> array.map(String::toInt) }
+        .map { it.sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
+}
+
+enum class Hand(val score: Int) { Rock(1), Paper(2), Scissors(3) }
+
+fun Char.toHand() = when (this) {
+    'X', 'A' -> Hand.Rock
+    'Y', 'B' -> Hand.Paper
+    'Z', 'C' -> Hand.Scissors
+    else -> throw IllegalArgumentException("Unknown hand: $this")
+}
+
+
+infix fun Hand.wins(other: Hand) = when (this) {
+    Hand.Rock -> other == Hand.Scissors
+    Hand.Paper -> other == Hand.Rock
+    Hand.Scissors -> other == Hand.Paper
+    else -> false
+}
+
+infix fun Hand.outcome(other: Hand): Int = when {
+    this wins other -> 6
+    other wins this -> 0
+    else -> 3
+}
+
+infix fun Hand.play(other: Hand): Int = score + outcome(other)
+
+val solve02: Solve<Int> = { data ->
+    data
+        .map { Pair(it.first().toHand(), it.last().toHand()) }
+        .sumOf { (a, b) -> a play b }
 }
